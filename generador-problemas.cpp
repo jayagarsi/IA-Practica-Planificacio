6f69b5ec543d7e650e85867c5a6a2fc432ceb586
Jaya Gracias <jaya.garcia@estudiantat.upc.edu>
2021-12-31 17:49:38 +0100

    usage al generador de problemes

--- generador-problemas.cpp
+++ generador-problemas.cpp
@@ -4,35 +4,40 @@
 #include <string>
 using namespace std;
 
-int main() {
-    cout << "Generador problemes aleatoris" << endl;
-    cout << "Extensio a generar fitxer? (basic, 1, 2, 3, 4)" << endl;
-    while (cin << e and (e != "basic" or e != "1" or e != "2" or e != "3" or e != "4"))
-    cout << "Opcio erronia escriu basic, 1, 2, 3 o 4" << endl;
-    string e;
-    cin << e;
+void usage() {
+    cout << "USAGE: ./generador-problemas -p ... -h ... -r ..." << endl;
+    cout << "            -p nivel de extension a generar (basic, 1, 2, 3, 4)" << endl;
+    cout << "            -h numero de habitaciones" << endl;
+    cout << "            -r numero de reservas" << endl;
+    exit(0);
+}
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 7) usage();
 
     ofstream output_file;
     output_file.open("random-problem.pddl");
 
-    switch (e) {
-        case "basic":
-            output_file << "(define (problem random-basic) (:domain hotel)" << endl;
-            break;
-        case "1":
-            output_file << "(define (problem random-extensio1) (:domain hotel)" << endl;
-            break;
-        case "2":
-            output_file << "(define (problem random-extensio2) (:domain hotel)" << endl;
-            break;
-        case "3":
-            output_file << "(define (problem random-extensio3) (:domain hotel)" << endl;
-            break;
-        case "4":
-            output_file << "(define (problem random-extensio4) (:domain hotel)" << endl;
-            break;        
-    }
+    string e = argv[2];
 
-    fichero_salida << "		(:objects ";	
+    if (e == "basic") 
+        output_file << "(define (problem random-basic) (:domain hotel)" << endl;
+
+    else if (e == "1")
+        output_file << "(define (problem random-extensio1) (:domain hotel)" << endl;
+
+    else if (e == "2")
+        output_file << "(define (problem random-extensio2) (:domain hotel)" << endl;
+
+    else if (e == "3") 
+        output_file << "(define (problem random-extensio3) (:domain hotel)" << endl;
+    
+    else if (e == "4")
+        output_file << "(define (problem random-extensio4) (:domain hotel)" << endl;    
+
+    else usage();
+
+    output_file << "		(:objects ";	
     
 }
